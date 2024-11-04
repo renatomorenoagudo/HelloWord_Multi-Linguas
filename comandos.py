6bd91fc10d66648c373e53da0a4418943298efae
curso bruno <renatomorenoagudo2@gmail.com>
2024-11-04 20:04:20 -0300

    Curso Bruno Rocha

---
+++ comandos.py
@@ -0,0 +1,30 @@
+no terminal do linux, no meu caso o bash:
+
+python -m site : mostra o sys.path mostrando quais os caminhos que o python 
+vai buscar os pacotes
+-vai mostrar tbm a pasta de usuario
+
+python - help 
+-mostra uma gma de comandos para mexer no terminal py 
+
+
+modulo tutle demo: diversas aplicaçoes demonstrativas visuais
+python -m tutledemo
+
+INTERPRETADOR PYTHON: SÓ DIGITAR PYTHON NO TERMINAL
+ELE VAI MUDAR A APARENCIA DAS LETRAS E MUDA PARA ">>>"
+- sistema fica em loop, não precisa colocar print, pqp ele sempre 
+vai imprimir ja
+-para sair é só digitar:
+EXIT()   ou CTRL+D 
+
+o help() mostra tutoriais, se vc colocar um objeto especifico dentro dos parenteses
+ele vai exemplificar e ensinar como lidar com aquele objeto  que pode ser um numero por 
+exemplo.. help(12)
+-posso entrar no gelp() e depois digitar modulos, p.ex. que ele vai mostrar
+todos modulo instalados no meu PC 
+
+ditiga Q pra sair 
+
+
+
