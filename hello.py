14da9afabfea857ba48a516de4b45feac6446804
Renato Moreno Agudo <113729018+renatomorenoagudo@users.noreply.github.com>
2024-11-07 19:57:53 -0300

    criado cabeçalho e programa simples de estudo
    criado programa hello world com 4 oçoes de linguagens (utilizando Elit e a tradução do olá mundo), que executa de acordo com a linguagem padrao do usuario.
    Posteriormente será criada uma função mais complexa que traduz utilizando outra forma mais eficiente.

---
+++ hello.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# # acima qdo usa ! (shibang) indica o caminho do python antes do programa rodar
+#para pesquisar o caminho, no termimal bash,digite which python.
+#este programa imprime o ola mundo!
+
+"""
+hello world multi linguas
+
+dependendo da lingua configurada no ambiente o progrma
+exibe a correspodente.
+
+Como Usar:
+
+Tenha avariável LANG devidamente configurada ex:
+      export LANG=pt_br
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+
+"""
+__version__="0.0.1"
+__author__="Renato Moreno"
+__license__="Unlicense"
+#'dander' significa que tem dois underlines antes e depois.
+
+#Code:
+import os  
+
+currente_language=os.getenv("LANG","en_US")[:5]
+
+msg="Hello, World!"
+if currente_language =="pt_BR":
+    msg ="Olá, Mundo!"
+elif currente_language=="it_IT":
+    msg="CIao, Mondo!"
+elif currente_language=="es_SP":
+    msg="Holla, Mundo!"
+elif currente_language=="es_SP":
+    msg="Bonjour, Monde!"
+
+print(msg)
