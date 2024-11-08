59cc2414d8bc3c5149c5e087202a928eaf6e9d6c
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-11-07 22:49:31 -0300

    crido o ambiente virtual venv e o .gitignore

--- comandos.py
+++ comandos.py
@@ -1,3 +1,6 @@
+""" passo a passo para comandos no terminal, desde para usar o github
+como para navegar nos arquivos
+
 no terminal do linux, no meu caso o bash:
 
 python -m site : mostra o sys.path mostrando quais os caminhos que o python 
@@ -26,5 +29,79 @@ todos modulo instalados no meu PC
 
 ditiga Q pra sair 
 
+;;;;;;;;;;;;;;;
+
+para criar um ambiente virtual no python para que  nao 
+prejudique os arquivos do py é importante criar um .venv
+
+digite no bash 
+   python -m venv .venv
+o .venv sera o nome da pasta a ser criada
+
+note que se digitar apenas 
+  ls 
+mostrara apenas os aequivos nomrmais
+
+mas se digitar ls -a , mostrara os ocultos, dentre eles o .venv
 
 
+para ver o que tem dentro do s.venv
+   ls .venv
+para ver o ambiente(bin(linux) ou scripts(windows))
+:
+     ls .venv/scripts
+aparecera os aquivos : pip, pip3, python, pythonw
+
+, a pasta .VENV está isolada do sistema, se der algum problema, posso delegar ela 
+e começar uma nova venv, pq os arquivos estarão dentro delas e isolados
+
+para a tivar o venv no terminal digite:
+
+    source .venv/Scripts/activate
+
+ou 
+    source .venv/bin/activate (linux)
+
+
+obs: aparecera (.venv) na mensagem do terminal
+
+e quando pesquisar 
+    which python
+aparecera outro caminho para o python, o caminho da .venv (virtual)
+observe que no caminho tem /.venv/Scripts/python no final
+que prova isso, essa maquina virtual.
+
+só tem q lembrar de executar o 
+source .venv/Scripts/activate
+para acessar o seu ambiente virtual e trabalhar
+e para confirmar so
+dar um which python para confirmar o caminho
+do python que estara rodando/salvando
+.......................
+para resolver o problema com o github
+referente essa nova pasta .venv
+pq a gente não deve mandar ela pro hub
+pra isso tem que criar um arquivo .gitignore
+para qdo de um "git status" ela não mostre mais 
+a pasta .venv
+
+assim 
+precisamos criar esse arquivo e listar "globs"
+que são padroes de listagem de arquivos, e assim 
+qualquer coisa que faça mach ele vai ignorar..
+
+assim dentro desse novo arquivo .gitignore só digitar .venv
+para que a pasta .venv seja ignorada pelo github
+na pesquisa git status.
+..........................
+toda vez que for fazer um commit de der erro de index.lock
+
+pode executar a remoção do index da seguinte forma:
+    rm -f .git/index.lock
+
+
+
+
+
+
+"""
